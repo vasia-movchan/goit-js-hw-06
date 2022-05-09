bbe1c2c75a6794dcec5ffffb3fd479758ad76414
Vasiliy Movchan <movchan.vasia@gmail.com>
2022-05-09 22:40:57 +0300

    task-01 done

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+const categories = document.querySelectorAll(".item");
+const numberCategories = categories.length;
+console.log(`Number of categories: ${numberCategories}`);
+console.log("");
+
+const test = categories.forEach((category) => {
+  const titleOfCategory = category.querySelector("h2").textContent;
+  const numberElementsOfCategory = category.querySelectorAll("li").length;
+  console.log(`Category: ${titleOfCategory}`);
+  console.log(`Elements: ${numberElementsOfCategory}`);
+  console.log("");
+});
