3ab5f4058f3b2d19512e0c3c4e937cb44d601f5c
Vasiliy Movchan <movchan.vasia@gmail.com>
2022-05-11 19:30:39 +0300

    task-02 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const liMarkup = ingredients
+  .map((ingredient) => {
+    const liElement = document.createElement("li");
+    liElement.textContent = ingredient;
+    liElement.classList.add("item");
+    return liElement.outerHTML;
+  })
+  .join("");
+
+const list = document.querySelector("#ingredients");
+list.innerHTML = liMarkup;
