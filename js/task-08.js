241c21d2431143c7c072b0961a7f84fc899cbba6
Vasiliy Movchan <movchan.vasia@gmail.com>
2022-05-14 21:34:03 +0300

    task-08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const { email, password } = form.elements;
+
+  if (email.value === "" || password.value === "") {
+    alert("Необхідно заповнити всі поля форми");
+  } else {
+    const user = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(user);
+    form.reset();
+  }
+}
