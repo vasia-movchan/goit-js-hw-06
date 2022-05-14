c86d0db88102bc39c7efe257bdd92146908e6840
Vasiliy Movchan <movchan.vasia@gmail.com>
2022-05-14 22:05:48 +0300

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const btnChangeColor = document.querySelector(".change-color");
+const body = document.body;
+const colorElement = document.querySelector(".color");
+
+btnChangeColor.addEventListener("click", () => {
+  const valueColor = getRandomHexColor();
+  body.style.backgroundColor = valueColor;
+  colorElement.textContent = valueColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
