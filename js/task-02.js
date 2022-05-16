b13527af6a804f753bb66509904baf56601ca9d0
Vasiliy Movchan <movchan.vasia@gmail.com>
2022-05-16 09:49:22 +0300

    bugfix task-02

--- js/task-02.js
+++ js/task-02.js
@@ -7,14 +7,12 @@ const ingredients = [
   "Condiments",
 ];
 
-const liMarkup = ingredients
-  .map((ingredient) => {
-    const liElement = document.createElement("li");
-    liElement.textContent = ingredient;
-    liElement.classList.add("item");
-    return liElement.outerHTML;
-  })
-  .join("");
+const liMarkup = ingredients.map((ingredient) => {
+  const liElement = document.createElement("li");
+  liElement.textContent = ingredient;
+  liElement.classList.add("item");
+  return liElement;
+});
 
 const list = document.querySelector("#ingredients");
-list.innerHTML = liMarkup;
+list.append(...liMarkup);
